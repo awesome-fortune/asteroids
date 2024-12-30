2bab49eec66180820429c95d3677c3d78c9deffe
awesome-fortune <mrfortune.maseko@gmail.com>
2024-12-30 06:18:37 +0200

    Add player bullet rate limiting

--- player.py
+++ player.py
@@ -5,6 +5,8 @@ from shot import Shot
 
 
 class Player(CircleShape):
+    timer = 0
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
@@ -38,10 +40,16 @@ class Player(CircleShape):
         if keys[pygame.K_SPACE]:
             self.shoot()
 
+        self.timer -= dt
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.timer > 0:
+            return
+
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
