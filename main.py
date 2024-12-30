22a59b338033756cd84dd967c24995562c84af48
awesome-fortune <mrfortune.maseko@gmail.com>
2024-12-30 13:51:14 +0200

    Implement splitting asteroids

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import sys
 import pygame
-from asteroid import Asteroid
-from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from shot import Shot
 
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = updatable
     Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    asteroid_field = AsteroidField()
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -36,13 +37,14 @@ def main():
             obj.update(dt)
 
         for asteroid in asteroids:
-            for shot in shots:
-                if shot.collides_with(asteroid):
-                    asteroid.kill()
-
-            if player.collides_with(asteroid):
-                print("Game over")
+            if asteroid.collides_with(player):
+                print("Game over!")
                 sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
@@ -50,6 +52,8 @@ def main():
             obj.draw(screen)
 
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
